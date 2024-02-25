2d4b94c35c5d7454cf09c6bec31b37055590edab
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-25 10:54:15 +0200

    Added navListData In constants File In utils Folder

---
+++ src/utils/constants.ts
@@ -0,0 +1,32 @@
+export const navListData = [
+  { id: 1, target: "home", name: "Home", icon: "bi-house-door", active: true },
+  {
+    id: 2,
+    target: "games",
+    name: "Games",
+    icon: "bi-window-stack",
+    active: false,
+  },
+  {
+    id: 2,
+    target: "stores",
+    name: "Stores",
+    icon: "bi-window-stack",
+    active: false,
+  },
+  {
+    id: 2,
+    target: "creators",
+    name: "Creators",
+    icon: "bi-window-stack",
+    active: false,
+  },
+  {
+    id: 3,
+    target: "library",
+    name: "My Library",
+    icon: "bi-heart",
+    active: false,
+  },
+  { id: 4, target: "bag", name: "My Bag", icon: "bi-bag", active: false },
+];
