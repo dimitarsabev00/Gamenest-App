9b132f8ef7ed6cd48a291d828a3f234b57da0a78
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-25 10:53:51 +0200

    Install "bootstrap-icons" Package

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./main.scss";
-
+import "bootstrap-icons/font/bootstrap-icons.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <>
     <App />
