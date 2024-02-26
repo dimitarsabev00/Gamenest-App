137cbbfa0ac091ed517f88775470cb8a51e0ceb3
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-26 15:08:43 +0200

    Render Home Screen In App Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Header, SideMenu } from "./components";
 import "./App.scss";
+import { Home } from "./screens";
+import { gamesData } from "./utils/constants";
+import { Game } from "./Types";
 
 const App = () => {
   const [active, setActive] = useState(false);
+  const [games, setGames] = useState<Game[]>([]);
+
   const handleToggleActive = () => {
     setActive((prev) => !prev);
   };
+
+  useEffect(() => {
+    setGames(gamesData);
+  }, []);
 
   return (
     <main className="main-screen-container">
       <SideMenu active={active} />
       <div className={`select-tab-content ${active ? "active" : undefined}`}>
         <Header toggleActive={handleToggleActive} />
+        <div className="contain-fluid">
+          {games && games.length > 0 && <Home games={games} />}
+        </div>
       </div>
     </main>
   );
