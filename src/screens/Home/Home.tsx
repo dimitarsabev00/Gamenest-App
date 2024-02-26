7d33e446007f95b133ce08eb14867059ab1e5d7f
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-26 15:07:03 +0200

    Refactor Home Screen

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,5 +1,37 @@
-const Home = () => {
-  return <div>Home</div>;
+import React from "react";
+import "./styles.scss";
+import { Game } from "../../Types";
+import { GameCard, GamesSwiper } from "../../components";
+
+type HomeProps = {
+  games: Game[];
+};
+
+const Home: React.FC<HomeProps> = ({ games }) => {
+  return (
+    <section id="home" className="home active">
+      <div className="container-fluid">
+        <div className="row">
+          <GamesSwiper games={games} />
+        </div>
+        <div className="row mb-4 mt-4 ">
+          <div className="col-lg-6">
+            <h2 className="sectionTitle">Top Popular Games</h2>
+          </div>
+          <div className="col-lg-6 d-flex justify-content-end align-items-center">
+            <a href="#" className="viewMore">
+              View More Games <i className="bi bi-arrow-right"></i>
+            </a>
+          </div>
+        </div>
+        <div className="row cards">
+          {games.slice(0, 4).map((game) => (
+            <GameCard key={game._id} game={game} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
