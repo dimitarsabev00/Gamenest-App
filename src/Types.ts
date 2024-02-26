e536a51ff11d1243fc193c6a88fec2440f2e833a
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-26 13:56:57 +0200

    Update Game Interface

--- src/Types.ts
+++ src/Types.ts
@@ -1,7 +1,12 @@
-export interface Game {
-  _id: string;
-  img?: string;
-  trailer: string;
+export interface  Game {
+  _id: number;
   title: string;
   description: string;
+  level: string;
+  category: string;
+  rating: number;
+  discount: number;
+  price: number;
+  trailer: string;
+  active: boolean;
 }
