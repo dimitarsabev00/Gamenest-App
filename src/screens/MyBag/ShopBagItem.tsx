34169f50b642dad212edef058e2cca77e796cb19
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-02-29 14:00:04 +0200

    Added RemoveItemFromBag Functionality In ShopBagItem Component

--- src/screens/MyBag/ShopBagItem.tsx
+++ src/screens/MyBag/ShopBagItem.tsx
@@ -1,11 +1,23 @@
 import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { RootState, setGeneralFields } from "../../store";
 import "./styles.scss";
 import { Game } from "../../Types";
 
 type ShopBagItemProps = { index: number; shopItem: Game };
 
 const ShopBagItem: React.FC<ShopBagItemProps> = ({ shopItem, index }) => {
- 
+  const myBag: Game[] = useSelector(
+    (state: RootState) => state.generalSlice.myBag
+  );
+  const dispatch = useDispatch();
+  const handleRemoveFromBag = (shopItem: Game) => {
+    dispatch(
+      setGeneralFields({
+        myBag: myBag.filter((item: Game) => item._id !== shopItem._id),
+      })
+    );
+  };
 
   return (
     <tr className="shopBagItem">
@@ -18,7 +30,7 @@ const ShopBagItem: React.FC<ShopBagItemProps> = ({ shopItem, index }) => {
       <td>{shopItem.discount * 100}%</td>
       <td>${(shopItem.price * (1 - shopItem.discount)).toFixed(2)}</td>
       <td>
-        <a href="#">
+        <a href="#" onClick={() => handleRemoveFromBag(shopItem)}>
           <i className="bi bi-trash"></i>
         </a>
       </td>
